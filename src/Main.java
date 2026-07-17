b6817efaf752f0cfff3a44cb1ef7712c6e8d9eb4
Sakshi <kashyapxSakshi@gmail.com>
2026-07-17 14:44:43 +0530

    feat: create Book class using OOP

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,14 @@
-import java.util.Scanner;
+import model.Book;
 
 public class Main {
 
-    // Method to display welcome message
-    public static void welcomeMessage() {
-        System.out.println("==================================");
-        System.out.println("   LIBRARY MANAGEMENT SYSTEM");
-        System.out.println("==================================");
-    }
-
-    // Method to calculate late fee
-    public static double calculateLateFee(int daysLate) {
-        double feePerDay = 5.0;
-        return daysLate * feePerDay;
-    }
-
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("===== Library Management System =====");
 
-        welcomeMessage();
+        Book book1 = new Book(101, "Java Programming", "James Gosling");
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        book1.displayBookDetails();
 
-        System.out.print("Enter Book ID: ");
-        int bookId = scanner.nextInt();
-
-        System.out.print("Enter number of late days: ");
-        int daysLate = scanner.nextInt();
-
-        double totalFee = calculateLateFee(daysLate);
-
-        System.out.println("\n========= Receipt =========");
-        System.out.println("Student Name : " + name);
-        System.out.println("Book ID      : " + bookId);
-        System.out.println("Late Days    : " + daysLate);
-        System.out.println("Late Fee     : ₹" + totalFee);
-
-        if (daysLate == 0) {
-            System.out.println("Status       : Returned On Time");
-        } else {
-            System.out.println("Status       : Late Return");
-        }
-
-        scanner.close();
     }
 }
